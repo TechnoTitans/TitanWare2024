d7529dd40d8a0b55478b32499d27c0913c96f8ba
Max <maxgoog06@outlook.com>
2024-01-06 16:12:08 -0500

    Added VendorDeps and CI

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "TitanWare2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "760480b1e7f47b82b0029086abfd548349fbc3ed";
+  public static final String GIT_DATE = "2024-01-06 15:51:06 EST";
+  public static final String GIT_BRANCH = "develop";
+  public static final String BUILD_DATE = "2024-01-06 15:58:41 EST";
+  public static final long BUILD_UNIX_TIME = 1704574721414L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
