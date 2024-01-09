e8c6b60678b5f3f0e6e1c293706476c5784785e6
harry.chen <harryxtchen@gmail.com>
2024-01-09 16:52:02 -0500

    Added new can ids

--- src/main/java/frc/robot/constants/HardwareConstants.java
+++ src/main/java/frc/robot/constants/HardwareConstants.java
@@ -6,8 +6,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.subsystems.drive.Swerve;
 import frc.robot.subsystems.drive.SwerveModule;
 
-import static frc.robot.constants.Constants.Swerve.TRACK_WIDTH_M;
-import static frc.robot.constants.Constants.Swerve.WHEEL_BASE_M;
+import static frc.robot.constants.Constants.Swerve.*;
 
 public class HardwareConstants {
     public record SwerveModuleConstants(
@@ -21,16 +20,16 @@ public class HardwareConstants {
             double turnEncoderOffset
     ) {
         public enum Hardware {
-            SDSMk4i_TalonFX_CANCoder
+            SDSMk4i_Falcon500_CANCoder
         }
 
         public static SwerveModule create(
                 final SwerveModuleConstants swerveModuleConstants,
                 final Constants.RobotMode currentMode,
                 final Swerve.OdometryThreadRunner odometryThreadRunner
-        ) {
+                ) {
             return switch(swerveModuleConstants.hardware) {
-                case SDSMk4i_TalonFX_CANCoder -> SwerveModule.Builder.SDSMK4iTalonFXCANCoder(
+                case SDSMk4i_Falcon500_CANCoder -> SwerveModule.Builder.SDSMK4iTalonFXCANCoder(
                         swerveModuleConstants.name,
                         new TalonFX(swerveModuleConstants.driveMotorId, swerveModuleConstants.moduleCANBus),
                         new TalonFX(swerveModuleConstants.turnMotorId, swerveModuleConstants.moduleCANBus),
@@ -53,44 +52,88 @@ public class HardwareConstants {
     public static final SwerveModuleConstants FRONT_LEFT_MODULE = new SwerveModuleConstants(
             "FrontLeft",
             RobotMap.CanivoreCANBus,
-            SwerveModuleConstants.Hardware.SDSMk4i_TalonFX_CANCoder,
+            SwerveModuleConstants.Hardware.SDSMk4i_Falcon500_CANCoder,
             new Translation2d(WHEEL_BASE_M / 2, TRACK_WIDTH_M / 2),
             1,
             2,
             3,
-            0
+            0.320556640625
     );
 
     public static final SwerveModuleConstants FRONT_RIGHT_MODULE = new SwerveModuleConstants(
             "FrontRight",
             RobotMap.CanivoreCANBus,
-            SwerveModuleConstants.Hardware.SDSMk4i_TalonFX_CANCoder,
+            SwerveModuleConstants.Hardware.SDSMk4i_Falcon500_CANCoder,
             new Translation2d(WHEEL_BASE_M / 2, -TRACK_WIDTH_M / 2),
             4,
             5,
             6,
-            0
+            0.33251953125
     );
 
     public static final SwerveModuleConstants BACK_LEFT_MODULE = new SwerveModuleConstants(
             "BackLeft",
             RobotMap.CanivoreCANBus,
-            SwerveModuleConstants.Hardware.SDSMk4i_TalonFX_CANCoder,
+            SwerveModuleConstants.Hardware.SDSMk4i_Falcon500_CANCoder,
             new Translation2d(-WHEEL_BASE_M / 2, TRACK_WIDTH_M / 2),
             7,
             8,
             9,
-            0
+            0.0478515625
     );
 
     public static final SwerveModuleConstants BACK_RIGHT_MODULE = new SwerveModuleConstants(
             "BackRight",
             RobotMap.CanivoreCANBus,
-            SwerveModuleConstants.Hardware.SDSMk4i_TalonFX_CANCoder,
+            SwerveModuleConstants.Hardware.SDSMk4i_Falcon500_CANCoder,
             new Translation2d(-WHEEL_BASE_M / 2, -TRACK_WIDTH_M / 2),
             10,
             11,
             12,
-            0
+            0.283203125
+    );
+
+    public record ElevatorConstants(
+            String verticalElevatorCANBus,
+            int verticalMainMotorId,
+            int verticalFollowerMotorId,
+            int verticalEncoderId,
+            int verticalLimitSwitchDIOChannel,
+            String horizontalElevatorCANBus,
+            int horizontalMotorId,
+            int horizontalEncoderId,
+            int horizontalLimitSwitchDIOChannel
+    ) {}
+
+    public static final ElevatorConstants ELEVATOR = new ElevatorConstants(
+            RobotMap.CanivoreCANBus,
+            14,
+            15,
+            16,
+            6,
+            RobotMap.RioCANBus,
+            17,
+            18,
+            8
+    );
+
+    public record ClawConstants(
+            String clawCANBus,
+            int clawMainWheelMotorId,
+            int clawFollowerWheelMotorId,
+            int clawOpenCloseMotorId,
+            int clawOpenCloseEncoderId,
+            int clawTiltMotorId,
+            int clawTiltEncoderId
+    ) {}
+
+    public static final ClawConstants CLAW = new ClawConstants(
+            RobotMap.RioCANBus,
+            19,
+            20,
+            21,
+            22,
+            23,
+            24
     );
 }
