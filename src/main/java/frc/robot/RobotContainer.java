400654a0ec997ad51978d6c2c38f6245c59cc892
Mqx <34967410+MqxS@users.noreply.github.com>
2024-03-05 17:42:28 -0500

    Merge superstructure into master (#42)
    * Added VendorDeps and CI
    
    * spotless was fired.
    
    * Removed spotless check from CI
    
    * Added Swerve, Gyro, and a lot of Utils.
    
    * Added swerve drive teleop as a functional command within Swerve... should work, I hope?
    
    * Added Tests
    
    * Upgrade to wpilib-2024 full season release, upgrade Phoenix6 and AdvantageKit to 2024 versions.
    
    * Super Jank Shooter code for prototype
    
    * Simple Motor Feedforward and CanID
    
    * Added & ran SysId, start using feedforward on the shooter with the characterized gains.
    
    * Change ShooterIOReal to use VelocityVoltage instead of bang bang control, still has feedforward just not using SimpleMotorFeedforward
    
    * Changed Sim to match real. Changed VelocityVoltage to VelocityTorqueCurrentFOC in Real. Removed useless variables in LoggedTunableNumber.
    
    * Changed voltage SysId to do torque current SysId instead. Got some mediocre gains but will try again after some discussion.
    
    * Finished tuning VelocityTorqueCurrentFOC for shooter prototype
    
    * Missed a required merge on HardwareConstants resulting in the removal of ShooterConstants from it
    
    * Added a TorqueCurrent limit to the bottom TalonFX
    
    * Unfinished changes for arm pivot and shooter.
    
    * Push code that killed a Falcon (TalonFX) 😰...
    
    * Refactored some control logic on the Shooter, also added Supply Current Limits and made all the limits more conservative.
    
    * Max House Things. ArmSim Config needs to be changed to match real.
    
    * Rename old references to refactored (renamed) class.
    
    * Updated Arm and Shooter simulation implementations to match latest revision of real robot. Still lots of work to do, but closer now...
    
    * This goal/state stuff kinda sucks... should be improved + fixed 😊.
    
    * Add WIP goals to the rest of superstructure.
    
    * Goals seem to work in sim. Uh. Real is different story rn.
    
    * Got working states for superstructure
    
    * Fixes #38. Removed all Followers and instead used same ControlRequest for both motors.
    
    ---------
    
    Co-authored-by: harry.chen <harryxtchen@gmail.com>
    Co-authored-by: HarryXChen <51322624+HarryXChen3@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,21 +1,25 @@
 package frc.robot;
 
-import com.ctre.phoenix6.SignalLogger;
 import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.Constants;
 import frc.robot.constants.HardwareConstants;
 import frc.robot.constants.RobotMap;
 import frc.robot.subsystems.drive.Swerve;
+import frc.robot.subsystems.superstructure.Superstructure;
+import frc.robot.subsystems.superstructure.arm.Arm;
+import frc.robot.subsystems.superstructure.shooter.Shooter;
 
 public class RobotContainer {
     public final PowerDistribution powerDistribution;
 
     public final Swerve swerve;
+    public final Arm arm;
+    public final Shooter shooter;
+
+    public final Superstructure superstructure;
 
     public final CommandXboxController driverController;
     public final CommandXboxController coDriverController;
@@ -36,18 +40,36 @@ public class RobotContainer {
                 HardwareConstants.BACK_RIGHT_MODULE
         );
 
+        this.arm = new Arm(Constants.RobotMode.REPLAY, HardwareConstants.ARM);
+        this.shooter = new Shooter(Constants.CURRENT_MODE, HardwareConstants.SHOOTER);
+
+        this.superstructure = new Superstructure(arm, shooter);
+
         this.driverController = new CommandXboxController(RobotMap.MainController);
         this.coDriverController = new CommandXboxController(RobotMap.CoController);
 
-        this.coDriverController.y().whileTrue(swerve.linearVoltageSysIdQuasistaticCommand(SysIdRoutine.Direction.kForward));
-        this.coDriverController.a().whileTrue(swerve.linearVoltageSysIdQuasistaticCommand(SysIdRoutine.Direction.kReverse));
-        this.coDriverController.b().whileTrue(swerve.linearVoltageSysIdDynamicCommand(SysIdRoutine.Direction.kForward));
-        this.coDriverController.x().whileTrue(swerve.linearVoltageSysIdDynamicCommand(SysIdRoutine.Direction.kReverse));
-
-        this.coDriverController.leftBumper().onTrue(Commands.run(SignalLogger::stop));
+        this.driverController.y().onTrue(
+                Commands.sequence(
+                        superstructure.toGoal(Superstructure.Goal.IDLE),
+//                        Commands.waitUntil(superstructure.atGoalTrigger),
+                        Commands.waitSeconds(6),
+                        superstructure.toGoal(Superstructure.Goal.SUBWOOFER),
+//                        Commands.waitUntil(superstructure.atGoalTrigger),
+                        Commands.waitUntil(superstructure.getShooter().atVelocityTrigger),
+                        Commands.waitSeconds(4),
+                        superstructure.toGoal(Superstructure.Goal.IDLE)
+                )
+        );
     }
 
     public Command getAutonomousCommand() {
-        return Commands.waitUntil(() -> !RobotState.isAutonomous());
+        return Commands.repeatingSequence(
+                superstructure.toGoal(Superstructure.Goal.IDLE),
+                Commands.waitUntil(superstructure.atGoalTrigger),
+                Commands.waitSeconds(4),
+                superstructure.toGoal(Superstructure.Goal.SUBWOOFER),
+                Commands.waitUntil(superstructure.atGoalTrigger),
+                Commands.waitSeconds(4)
+        );
     }
 }
