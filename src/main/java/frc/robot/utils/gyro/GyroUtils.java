fec456753abb0f58db7f48ff383b1f83c559a843
Mqx <34967410+MqxS@users.noreply.github.com>
2024-08-18 18:03:32 -0500

    Refactor and fix gyroutils (#82)
    * Haven't fixed robotPose2dToPose2dWithGyro but other methods have been removed.
    
    * Changed Mount Pose so hopefully GyroUtils stuff works correctly now.

--- src/main/java/frc/robot/utils/gyro/GyroUtils.java
+++ src/main/java/frc/robot/utils/gyro/GyroUtils.java
@@ -1,30 +1,11 @@
 package frc.robot.utils.gyro;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.*;
 import frc.robot.subsystems.drive.constants.SwerveConstants;
-
-import java.util.function.Supplier;
 
 public class GyroUtils {
     private static final double wheelBaseMeters = SwerveConstants.Config.wheelBaseMeters();
     private static final double trackWidthMeters = SwerveConstants.Config.trackWidthMeters();
-
-    public static Rotation2d withAngleModulus(final Supplier<Rotation2d> rotation2dSupplier) {
-        return Rotation2d.fromRadians(MathUtil.angleModulus(rotation2dSupplier.get().getRadians()));
-    }
-
-    public static double getAsDoubleDeg(final Rotation2d rotation2d) {
-        return rotation2d.getDegrees();
-    }
-
-    public static double getAsDoubleDeg(final Supplier<Rotation2d> rotation2dSupplier) {
-        return getAsDoubleDeg(rotation2dSupplier.get());
-    }
-
-    public static double getAsAngleModdedDoubleDeg(final Supplier<Rotation2d> rotation2dSupplier) {
-        return getAsDoubleDeg(withAngleModulus(rotation2dSupplier));
-    }
 
     public static Pose3d robotPose2dToPose3dWithGyro(final Pose2d pose2d, final Rotation3d gyroRotation) {
         return new Pose3d(pose2d)
@@ -36,29 +17,5 @@ public class GyroUtils {
                         0, 0, Math.abs(gyroRotation.getX()) * trackWidthMeters * 0.5,
                         gyroRotation.getX(), 0, 0
                 ));
-    }
-
-    /**
-     * Convert a Roll, Pitch, and Yaw into a {@link Rotation3d} object.
-     * @param roll the roll (rad)
-     * @param pitch the pitch (rad)
-     * @param yaw the yaw (rad)
-     * @return the {@link Rotation3d} describing the supplied euler angles
-     * @see Rotation3d
-     */
-    public static Rotation3d rpyToRotation3d(final double roll, final double pitch, final double yaw) {
-        return new Rotation3d(roll, pitch, yaw);
-    }
-
-    /**
-     * Convert Roll, Pitch, and Yaw {@link Rotation2d}s into a {@link Rotation3d} object.
-     * @param roll the roll {@link Rotation2d}
-     * @param pitch the pitch {@link Rotation2d}
-     * @param yaw the yaw {@link Rotation2d}
-     * @return the {@link Rotation3d} describing the supplied euler angles
-     * @see Rotation3d
-     */
-    public static Rotation3d rpyToRotation3d(final Rotation2d roll, final Rotation2d pitch, final Rotation2d yaw) {
-        return rpyToRotation3d(roll.getRadians(), pitch.getRadians(), yaw.getRadians());
     }
 }
